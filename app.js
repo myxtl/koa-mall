ad95ca32a1ea67720e12f13bb22cf389a557f947
myxtl <myxtl@foxmail.com>
2020-06-01 08:42:46 +0800

    log

--- app.js
+++ app.js
@@ -7,20 +7,22 @@ const Koa = require('koa')
 const app = new Koa()
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
-const logger = require('koa-logger')
 const KoaJwt = require('koa-jwt')
-const {
-    staticDir,
-    jwt
-} = require('./config/default')
 const error = require('./middlewares/error')
 const token = require('./middlewares/token')
+const { staticDir, jwt } = require('./config/default')
+const { logger, accessLogger } = require('./utils/logs');
 // 跨域
 app.use(cors());
 
 app.use(bodyParser());
 
-// error handler
+// logs
+app.use(accessLogger());
+
+app.on('error', err => {
+    logger.error(err);
+});
 
 
 // middlewares
@@ -28,14 +30,6 @@ app.use(error)
 // 验证token是否过期或失效
 // app.use(token)
 
-// logger
-app.use(logger())
-app.use(async (ctx, next) => {
-    const start = new Date()
-    await next()
-    const ms = new Date() - start
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
 
 // 
 // routes
